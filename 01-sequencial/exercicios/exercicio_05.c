9aae890dc9c619aab6366bf5e832cdd5512a5c49
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-04-28 09:52:37 -0300

    exercicio_05.c
    Soma

---
+++ 01-sequencial/exercicios/exercicio_05.c
@@ -0,0 +1,20 @@
+// Soma
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y, soma;
+
+    printf("\nInforme o valor de X: ");
+    scanf("%d", &x);
+
+    printf("Informe o valor de Y: ");
+    scanf("%d", &y);
+
+    soma = x + y;
+
+    printf("\nA soma total e de: %d", soma);
+
+    return (0);
+}
