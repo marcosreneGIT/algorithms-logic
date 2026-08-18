2c7050a54e92b0d3b1b45af8f8c143a84ff0740c
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2026-08-18 15:51:46 -0300

    exem-08
    for

---
+++ 03-repeticao/exemplos/exem-08.c
@@ -0,0 +1,16 @@
+// for
+
+#include <stdio.h>
+
+int main(void){
+
+    for (int i = 1; i <= 5; i++){
+        printf("Valor de i: %d\n", i);
+    }
+
+    for (int j = 5; j >= 1; j--){
+         printf("Valor de j: %d\n", j);
+    }
+
+    return 0;
+}
